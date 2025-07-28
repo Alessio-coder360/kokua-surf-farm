033315a6c98065ff1642448c5e2f85f4a41abb99
Alessio-coder360 <alessiocardone732@gmail.com>
2025-07-28 14:22:34 +0200

    Create update-programma.js
    aggiunto update-programma.js

---
+++ netlify/functions/update-programma.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const { password, programma } = JSON.parse(event.body);
+
+    // Proteggi con la stessa password admin (imposta su Netlify come variabile ambiente)
+    if (password !== process.env.ADMIN_PASSWORD) {
+      return { statusCode: 401, body: 'Unauthorized' };
+    }
+
+    // Percorso assoluto al file programma.json
+    const filePath = path.join(__dirname, '../../data/programma.json');
+    fs.writeFileSync(filePath, JSON.stringify(programma, null, 2), 'utf8');
+
+    return { statusCode: 200, body: 'OK' };
+  } catch (err) {
+    return { statusCode: 500, body: 'Errore: ' + err.message };
+  }
+};
